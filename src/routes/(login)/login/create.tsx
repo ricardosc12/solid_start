6c0ad3f1e49727551378732aa1d53baed05ca70a
Ricardo <ricardo.spinola9@gmail.com>
2023-02-25 20:11:17 -0300

    remove animation

--- src/routes/(login)/login/create.tsx
+++ src/routes/(login)/login/create.tsx
@@ -1,5 +1,3 @@
-import LoginAnimation from "~/components/LoginAnimations"
-
 export default function Create() {
   return (
     <><h2>Create</h2></>
--- src/routes/(login)/login/sign.tsx
+++ src/routes/(login)/login/sign.tsx
@@ -1,5 +1,3 @@
-import LoginAnimation from "~/components/LoginAnimations"
-
 export default function Sign() {
   return (
     <><h2>Sign</h2></>
